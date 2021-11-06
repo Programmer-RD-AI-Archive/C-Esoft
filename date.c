47c8b301b7f76b7e27e75480e6cb127e67e5e6ad
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-06 09:26:00 +0530

    Delete date.c

--- date.c
+++
@@ -1,8 +0,0 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-int main() {
-    int d,m,y;
-    printf("Enter Date of Birth : ");
-    scanf("%d-%d-%d",&d,&m,&y);
-    printf("%d-%d-%d is your b day",d,m,y);
-}
