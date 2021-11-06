6f23244ac5ae8d6192a6a981d173030f0b89e9aa
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-06 09:25:44 +0530

    Delete b.c

--- b.c
+++
@@ -1,9 +0,0 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-int main() {
-    printf("New Line is next \n ");
-    printf("This is the new line");
-    printf("\n");
-    printf("Back Space is next \b");
-    printf("This is after the back space");
-}
