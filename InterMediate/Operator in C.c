2f13b7a416bc1923f819a4bc7075c63ac8f5bccd
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-06 09:28:50 +0530

    Add files via upload

---
+++ InterMediate/Operator in C.c
@@ -0,0 +1,9 @@
+// Operator in C
+#include <stdio.h>
+int main() {
+    // + - Add
+    // - - Sub or Negatives numbers
+    // * - Multiplication
+    // / - Vision (Same/same = same | difference/same = difference)
+    // % - Modulas (Remainder)
+}
