0aa9cc9fac62f9ee1a9d54bfdf80e4ed9fc40f65
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2022-01-07 20:51:17 -0800

    Add files via upload

---
+++ Advanced/Array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int billy[5] = {1, 2, 3, 4, 5};
+    // billy[0] = 959;
+    // billy[1] = 599;
+    // billy[2] = 469;
+    // billy[3] = 569;
+    // billy[4] = 9;
+    printf(billy);
+}
