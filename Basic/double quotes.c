e35adedc7b18b750b17e44fd37f55631be22feb1
Programmer-RD-AI <go2ranuga@gmail.com>
2022-03-05 10:10:28 +0530

    Basic Clean Up
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Basic/double quotes.c
@@ -0,0 +1,6 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int a = 4;
+    printf("Testing C \"Code\"");
+}
---
+++ Basic/point_2f.c
@@ -0,0 +1,7 @@
+// Decimal converting like in python .2f
+#include <stdio.h>
+int main()
+{
+    const float a = 34.56254;
+    printf("a = %.2f", a);
+}
