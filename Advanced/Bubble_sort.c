fb7238ff0a2a7cb7dcbf846759c3c6c3259042d3
Programmer-RD-AI <go2ranuga@gmail.com>
2022-03-05 09:58:01 +0530

    Update Bubble_sort.c
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Advanced/Bubble_sort.c
+++ Advanced/Bubble_sort.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-
+#define SIZE 5
+int arr[SIZE] = {1, 2, 3, 4, 5};
+void swap(int i)
+{
+    int temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = temp;
+}
+void BubbleSort()
+{
+    int target = SIZE - 1;
+    while (target > 0)
+    {
+        int i;
+        for (i = 0; i < target; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(i);
+            }
+        }
+    }
+}
